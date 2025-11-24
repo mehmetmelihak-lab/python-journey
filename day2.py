12774a11f6d454395376e7f80b6cec02e4f53989
Mehmet Melih AK <mehmetmelihak@gmail.com>
2025-11-24 17:02:15 +0300

    Day 2 - Variables and data types

---
+++ day2.py
@@ -0,0 +1,9 @@
+name = 'Melih'
+age=19
+city='Istanbul'
+print(f'Hi my name is {name} im {age} and im from {city} ')
+#lists:
+programming_languages=['Python','C++','Java']
+print(programming_languages[0])
+programming_languages.append('JavaScript')
+print(programming_languages)
