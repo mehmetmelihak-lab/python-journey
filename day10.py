7548e7008ccefb3b894f417efb7b07cfd2955e5d
Mehmet Melih AK <mehmetmelihak@gmail.com>
2025-12-07 18:51:27 +0300

    Fix password generator logic

--- day10.py
+++ day10.py
@@ -1,25 +1,30 @@
 import random
+
 def password_generator():
-    length=int(input('Please enter the length of your desired password:'))
-    capital=input('Do you want upper case letters  ? yes or no please:').lower()
-    miniscule=input('Do you want lower case letters ? yes or no please:').lower()
-    number=input('Do you want numbers ? yes or no please:').lower()
-    special_letter=input('Do you want special letters ? yes or no please:').lower()
-    pool=""
-    if capital=='yes':
-        pool+='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    if miniscule=='yes':
-        pool+='abcdefghijklmnopqrstuvwxyz'
-    if number=='yes':
-        pool+='1234567890'
-    if special_letter=='yes':
-        pool+='!@#$%^&*()_-+='
-    if pool=="":
-        print('There is an error try again')
+    length = int(input('Please enter the length of your desired password: '))
+
+    capital = input('Do you want upper case letters? yes or no: ').lower()
+    miniscule = input('Do you want lower case letters? yes or no: ').lower()
+    number = input('Do you want numbers? yes or no: ').lower()
+    special_letter = input('Do you want special letters? yes or no: ').lower()
+
+    pool = ""
+
+    if capital == 'yes':
+        pool += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if miniscule == 'yes':
+        pool += 'abcdefghijklmnopqrstuvwxyz'
+    if number == 'yes':
+        pool += '0123456789'
+    if special_letter == 'yes':
+        pool += '!@#$%^&*()_-+='
+
+    if pool == "":
+        print("Error: You must select at least one character type.")
         return
-    
-    password="".join(random.choices(pool,k=length))
-    print(f'Generated password={password}')
+
+    password = "".join(random.choices(pool, k=length))
+    print(f'Generated password = {password}')
 
     
         
